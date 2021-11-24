a6ba7b41d5357014a20f950130ca64a8bb9fb8c1
Henrique <henriqueaureliocsilva@gmail.com>
2021-11-24 18:24:16 -0300

    feat: lack of implementing context

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { GlobalStyle } from './styles/global'
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+import { NewTransactionModal } from './components/NewTransactionModal'
+
+Modal.setAppElement('#root')
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      />
+      <GlobalStyle />
+    </>
+  )
+}
