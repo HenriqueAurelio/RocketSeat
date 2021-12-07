e7e54a588ad7a164b7408ef3b49ab7ce506e24eb
Henrique <henriqueaureliocsilva@gmail.com>
2021-12-07 19:13:45 -0300

    feat:first call by serverside and api from stripe implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
-export default function Home() {
+
+interface homeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+export default function Home({ product }: homeProps) {
   return (
     <>
       <Head>
@@ -16,12 +25,30 @@ export default function Home() {
           <p>
             Get access to all the publicaitons
             <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K4B8gEtCstiIJS73Kv2gboL')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+  }
 }
