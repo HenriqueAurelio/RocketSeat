ba51a6954f6b0c24617083b66359dc5747414b82
Henrique <henriqueaureliocsilva@gmail.com>
2021-12-08 15:17:10 -0300

    fix: fixed login by github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const [session] = useSession()
+  const { data: session } = useSession()
 
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Sign in with github
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
