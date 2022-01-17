f7648e2cf564a778b9bc1baba8f0053d37ceaeea
Henrique <henriqueaureliocsilva@gmail.com>
2022-01-17 00:35:29 -0300

    feat:fixing error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { fauna } from '../../../services/fauna'
-
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -18,32 +17,40 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    // async session(session) {
-    //   try {
-    //     const userActiveSubscription = await fauna.query(
-    //       q.Get(
-    //         q.Intersection([
-    //           q.Match(
-    //             q.Index('subscription_by_user_ref'),
-    //             q.Select(
-    //               'ref',
-    //               q.Get(
-    //                 q.Match(
-    //                   q.Index('user_by_email'),
-    //                   q.Casefold(session.user.email)
-    //                 )
-    //               )
-    //             )
-    //           ),
-    //           q.Match(q.Index('subscription_by_status'), 'active'),
-    //         ])
-    //       )
-    //     )
-    //     return { ...session, activeSubscription: userActiveSubscription }
-    //   } catch {
-    //     return { ...session, activeSubscription: null }
-    //   }
-    // },
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch (err) {
+        return {
+          ...session,
+          activeSubscription: null,
+          err,
+        }
+      }
+    },
     async signIn({ user, account, profile }) {
       const { email } = user
       try {
